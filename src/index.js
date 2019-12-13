b4dfe70adb059fbe5af653387b61ae3ad735a33c
linh-pham-nz <linh.pham.nz@gmail.com>
2019-12-13 15:43:18 +1300

    start declaring variables

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import { csv } from 'd3';
+import { select, csv } from 'd3';
+
+const svg = select('svg')
+
+const width = +svg.attr('width')
+const height = +svg.attr('height')
 
 csv('data.csv').then(data => {
     data.forEach(d => {
