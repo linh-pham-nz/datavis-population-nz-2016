ed446b1fc39dbe70c8267c902ca32fc0f295a396
linh-pham-nz <linh.pham.nz@gmail.com>
2019-12-13 16:01:19 +1300

    set inner measurements and margins

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ const svg = select('svg')
 const width = +svg.attr('width')
 const height = +svg.attr('height')
 
+const render = data => {
+    const margin = { top: 40, right: 40, bottom: 40, left: 40 }
+    const innerWidth = width - margin.left - margin.right
+    const innerHeight = height - margin.top - margin.bottom
+
+}
+
 csv('data.csv').then(data => {
     data.forEach(d => {
         d.total = +d.total
     })
-    // render(data)
+    render(data)
     console.log(data)
 })
