6bcf2f7caec273a913d7fadd39f3f04ce8d6afb1
linh-pham-nz <linh.pham.nz@gmail.com>
2019-12-13 17:46:37 +1300

    declare xValue and xScale

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { select, csv } from 'd3';
+import { select, csv, scaleBand } from 'd3';
 
 const svg = select('svg')
 
@@ -6,9 +6,16 @@ const width = +svg.attr('width')
 const height = +svg.attr('height')
 
 const render = data => {
+    const xValue = d => d.ageGroup
+
     const margin = { top: 40, right: 40, bottom: 40, left: 40 }
     const innerWidth = width - margin.left - margin.right
     const innerHeight = height - margin.top - margin.bottom
+
+    const xScale = scaleBand()
+        .domain(data.map(xValue))
+        .range([0, innerWidth])
+    // console.log(xScale.domain())
 
 }
 
