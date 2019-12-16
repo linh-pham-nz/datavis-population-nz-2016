b58f9fe4087b6bd69a06e4e22457c2b3a7ffbd30
linh-pham-nz <linh.pham.nz@gmail.com>
2019-12-16 21:20:05 +1300

    work on xAxisLabel

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ const height = +svg.attr('height')
 
 const render = data => {
     const xValue = d => d.ageGroup
+    const xAxisLabel = 'Age Group'
+
     const yValue = d => d.total
-    const margin = { top: 40, right: 40, bottom: 40, left: 100 }
+    const margin = { top: 40, right: 40, bottom: 100, left: 100 }
     const innerWidth = width - margin.left - margin.right
     const innerHeight = height - margin.top - margin.bottom
 
@@ -27,12 +29,19 @@ const render = data => {
     g.append('g').call(axisLeft(yScale))
     
     const xAxis = axisBottom(xScale)
-        .tickSize(-innerHeight)
-        .tickPadding(10)
+        // .tickSize(-innerHeight)
+        .tickPadding(5)
         
     const xAxisG = g.append('g')
         .call(xAxis)
         .attr('transform', `translate(0, ${innerHeight})`)
+    
+    xAxisG.append('text')
+        .attr('y', 60)
+        .attr('x', innerWidth/2)
+        .attr('fill', 'black')
+        .attr('font-size', 30)
+        .text(xAxisLabel)
 
     g.selectAll('circle')
         .data(data)
